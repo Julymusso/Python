3b6aebd92fec7274c693d1c5c2a8fb4133fb8a5e
Juliana Musso <juliana@Flexa>
2024-04-30 17:56:04 -0300

    Add aulas 65, 66, 67

--- secao4/aula65-functions.py
+++ secao4/aula65-functions.py
@@ -1,86 +1,17 @@
-﻿# # cpf = '36440847007'  # Esse CPF gera o primeiro dígito como 10 (0)
-# import re
-# import sys
+﻿'''
+Introdução às funções (def) em Python
+Funções são trechos de código usados para
+replicar determinada ação ao longo do seu código.
+Elas podem receber valores para parâmetros (argumentos)
+e retornoar um valor específico.
+Por padrão, funções Python retornam None (nada).
+'''
 
-# # cpf_enviado_usuario = '746.824.890-70' \
-# #     .replace('.', '') \
-# #     .replace(' ', '') \
-# #     .replace('-', '')
-# entrada = input('CPF [746.824.890-70]: ')
-# cpf_enviado_usuario = re.sub(
-#     r'[^0-9]',
-#     '',
-#     entrada
-# )
+def Print(a, b, c, d):
+    print(a)
+    print(b)
+    print(c)
+    print(d)
 
-# entrada_e_sequencial = entrada == entrada[0] * len(entrada)
-
-# if entrada_e_sequencial:
-#     print('Você enviou dados sequenciais.')
-#     sys.exit()
-
-# nove_digitos = cpf_enviado_usuario[:9]
-# contador_regressivo_1 = 10
-
-# resultado_digito_1 = 0
-# for digito in nove_digitos:
-#     resultado_digito_1 += int(digito) * contador_regressivo_1
-#     contador_regressivo_1 -= 1
-# digito_1 = (resultado_digito_1 * 10) % 11
-# digito_1 = digito_1 if digito_1 <= 9 else 0
-
-# dez_digitos = nove_digitos + str(digito_1)
-# contador_regressivo_2 = 11
-
-# resultado_digito_2 = 0
-# for digito in dez_digitos:
-#     resultado_digito_2 += int(digito) * contador_regressivo_2
-#     contador_regressivo_2 -= 1
-# digito_2 = (resultado_digito_2 * 10) % 11
-# digito_2 = digito_2 if digito_2 <= 9 else 0
-
-# cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
-
-# if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
-#     print(f'{cpf_enviado_usuario} é válido')
-# else:
-#     print('CPF inválido')
-
-import random
-
-cpf = ''
-for i in range(9):
-    cpf += str(random.randint(0, 9))
-
-#PRIMEIRO DÍGITO DO CPF
-i=10
-soma = 0
-for digito in cpf:
-    numero = int(digito)*i
-    soma += numero
-    i -= 1
-
-soma *= 10
-resto = soma % 11
-if resto < 9:
-    cpf=cpf+str(resto)
-else:
-    cpf=cpf+str(0)
-
-#SEGUNDO DÍGITO DO CPF
-i=11
-soma = 0
-for digito in cpf:
-    numero = int(digito)*i
-    soma += numero
-    i -= 1
-
-soma *= 10
-resto = soma % 11
-if resto < 9:
-    cpf=cpf+str(resto)
-else:
-    cpf=cpf+str(0)
-
-#COPARE CPF COM CPF VALIDO
-print(cpf)
+Print('Juliana', '33', 'Cristã', 'SRE')
+Print('Batata', '24', 'Sala', 'Doctor')
---
+++ secao4/aula66-argumentos.py
@@ -0,0 +1,14 @@
+﻿'''
+Argumentos nomeados e não nomeados em função Python
+Argumento nomeado tem nome com sinal de igual
+Argumento não nomeado recebe apenas o argumento (valor) = Argumento posicional
+'''
+
+def soma(x, y, z):
+    #Definição
+    print(f'{x=} y={y} z={z}', '|', 'x + y =', x + y)
+
+#print(soma) #Mostra posição da função na memória
+soma(1, 2, 3)
+soma(y=2, x=1, z=3)
+soma(1, 2, z=3)
