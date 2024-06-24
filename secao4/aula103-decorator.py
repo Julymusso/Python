0f6b41f07636923e216edd6adc06e965101298f9
Juliana Musso <juliana@Flexa>
2024-06-24 18:07:57 -0300

    freevar, nolocal var, decorator

---
+++ secao4/aula103-decorator.py
@@ -0,0 +1,27 @@
+﻿# Funções decoradoras e decoradores
+# Decorar = Adicionar / Remover/ Restringir / Alterar
+# Funções decoradoras são funções que decoram outras funções
+# Decoradores são usados para fazer o Python
+# usar as funções decoradoras em outras funções.
+
+def create_function(func):
+    def internal(*args, **kwargs):
+        print('Função será decorada')
+        for arg in args:
+            is_string(arg)
+        print(f'O resultado é {func(*args, **kwargs)}')
+        print('Função decorada, mas não executada ainda')
+        return func(*args, **kwargs)
+    return internal
+
+def inverte_string(string):
+    return string[::-1]
+
+def is_string(param):
+    if not isinstance(param, str):
+        raise TypeError('Param deve ser uma string')
+
+inverte_string_checa_param = create_function(inverte_string)
+
+print(inverte_string_checa_param('Juliana'))
+print('Agora foi executado')
---
+++ secao4/aula104-decoradores.py
@@ -0,0 +1,29 @@
+﻿# Funções decoradoras e decoradores
+# Decorar = Adicionar / Remover/ Restringir / Alterar
+# Funções decoradoras são funções que decoram outras funções
+# Decoradores são usados para fazer o Python
+# usar as funções decoradoras em outras funções.
+# Decoradores são "Syntax Sugar"
+
+def create_function(func):
+    def internal(*args, **kwargs):
+        print('Função será decorada')
+        for arg in args:
+            is_string(arg)
+        print(f'O resultado é {func(*args, **kwargs)}')
+        print('Função decorada, mas não executada ainda')
+        return func(*args, **kwargs)
+    return internal
+
+@create_function
+def inverte_string(string):
+    print(f'O nome da função é {inverte_string.__name__}')
+    return string[::-1]
+
+def is_string(param):
+    if not isinstance(param, str):
+        raise TypeError('Param deve ser uma string')
+
+
+print(inverte_string('Juliana'))
+print('Agora foi executado')
