ac965088fc4fb45deeaf455a566bc5bb53820ffb
Juliana Musso <juliana@Flexa>
2024-07-18 11:56:24 -0300

    secao 5. Classe, Métodos, self

---
+++ secao5/aula122-class-self.py
@@ -0,0 +1,17 @@
+﻿# Métodos em instâncias de classes Python
+
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+        
+    def acelerar(self):
+        print(f'{self.nome} acelerou')
+        
+
+fusca = Carro('Fusca')
+print(fusca.nome)
+fusca.acelerar() # Atributo de instância
+
+celta = Carro('Celta')
+print(celta.nome)
+fusca.acelerar() # Atributo de instância
